f7779026f8acd499e7e4bb9c915432a26848b255
Flan <jamioflan@hotmail.co.uk>
2023-11-29 15:34:36 +0100

    Add 3rd person poses for guns

--- src/main/java/com/flansmod/client/render/FlanClientItemExtensions.java
+++ src/main/java/com/flansmod/client/render/FlanClientItemExtensions.java
@@ -1,7 +1,13 @@
 package com.flansmod.client.render;
 
+import com.flansmod.common.item.GunItem;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.Nullable;
 
 public class FlanClientItemExtensions implements IClientItemExtensions
 {
@@ -16,6 +22,15 @@ public class FlanClientItemExtensions implements IClientItemExtensions
     {
         FlanModelRegistration.PreRegisterRenderer(item, renderer);
         return new FlanClientItemExtensions(renderer);
+    }
+
+    @Override
+    @Nullable
+    public HumanoidModel.ArmPose getArmPose(LivingEntity entityLiving, InteractionHand hand, ItemStack itemStack)
+    {
+        if(itemStack.getItem() instanceof GunItem)
+            return HumanoidModel.ArmPose.BOW_AND_ARROW;
+        return HumanoidModel.ArmPose.ITEM;
     }
 
     @Override
